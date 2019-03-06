721d91c95bc6cc9cb75f1119153266c8087ed6e4
Dirk Hornung <dirkhornung91@gmail.com>
2019-03-06 20:29:32 +0100

    dynamical round circle

--- components/SDGCircle.js
+++ components/SDGCircle.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import styled from 'styled-components'
 
-const Circle = styled.View`
+const Circle = styled(View)`
   flex: 1;
   background-color: red;
-  border-radius: 50;
 `
 
 export class SDGCircle extends Component {
+  state = { width: 0 }
+
+  handleLayout = ({ nativeEvent }) => {
+    this.setState({ width: nativeEvent.layout.width })
+  }
+
   render() {
-    return <Circle onLayout={(layout) => console.log(layout)} />
+    return <Circle onLayout={this.handleLayout} style={{borderRadius: Math.floor(this.state.width/2)}} />
   }
 }
