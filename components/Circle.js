fe25be3666d649b41470628276d145399c38ee23
Dirk Hornung <dirkhornung91@gmail.com>
2019-03-08 18:28:32 +0100

    added sdgcircle with orbiting SDGs

--- components/Circle.js
+++ components/Circle.js
@@ -3,8 +3,11 @@ import { Text, View } from 'react-native'
 import styled from 'styled-components'
 
 const StyledCircle = styled(View)`
-  flex: 1;
+  position: absolute;
+  width: ${props => props.radius*2};
+  height: ${props => props.radius*2};
   background-color: ${props => props.color};
+  border-radius: ${props => props.radius};
 `
 
 export default class Circle extends Component {
@@ -18,8 +21,10 @@ export default class Circle extends Component {
     return (
       <StyledCircle
         color={this.props.color}
+        style={{...this.props.style}}
+        radius={this.props.radius}
         onLayout={this.handleLayout}
-        style={{borderRadius: Math.floor(this.state.width/2)}}
+        /* style={{borderRadius: Math.floor(this.state.width/2)}} */
       />
     )
   }
--- components/SDGCircle.js
+++ components/SDGCircle.js
@@ -4,15 +4,82 @@ import styled from 'styled-components'
 import Circle from './Circle'
 
 const Container = styled.View`
-  flex: 1;
+  margin: auto;
+  width: 180px;
+  height: 180px;
+  position: relative;
+  background-color: #ac5;
 `
 
+const circles = [{
+  color: 'red'
+}, {
+  color: 'blue'
+}, {
+  color: 'green'
+}, {
+  color: 'yellow'
+}, {
+  color: 'red'
+}, {
+  color: 'blue'
+}, {
+  color: 'green'
+}, {
+  color: 'yellow'
+}, {
+  color: 'red'
+}, {
+  color: 'blue'
+}, {
+  color: 'green'
+}, {
+  color: 'yellow'
+}, {
+  color: 'red'
+}, {
+  color: 'blue'
+}, {
+  color: 'green'
+}, {
+  color: 'yellow'
+}, {
+  color: 'red'
+}]
+
+
 export default class SDGCircle extends Component {
+  state = {
+    deltaTheta: 360/circles.length,
+    Radius: 0, // radius of center circle (contaienr)
+    radius: 25, // radius of orbiting circles
+    container: { height: 0, width: 0 }
+  }
+
+  offset = () => parseInt(this.state.container.width/2)-this.state.radius
+  xPos = (i) => Math.sin((i*this.state.deltaTheta) * (Math.PI/180))*this.state.Radius+this.offset()
+  yPos = (i) => Math.cos((i*this.state.deltaTheta) * (Math.PI/180))*this.state.Radius+this.offset()
+
+  handleLayout = ({ nativeEvent }) => {
+    this.setState({
+      Radius: nativeEvent.layout.width,
+      container: {
+        height: nativeEvent.layout.height,
+        width: nativeEvent.layout.width
+      }
+    })
+  }
+
   render() {
     return (
-      <Container>
-        <Circle color="red" />
-        <Circle color="blue" />
+      <Container onLayout={this.handleLayout}>
+        {circles.map((circle, index) => (
+          <Circle
+            color={circle.color}
+            radius={this.state.radius}
+            style={{left: this.xPos(index), top: this.yPos(index)}}
+          />)
+                    )}
       </Container>
     )
   }
