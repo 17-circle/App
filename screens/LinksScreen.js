40ea81763d7e849a0fdc41b452f9af241665ed20
Dirk Hornung <dirkhornung91@gmail.com>
2019-03-23 17:39:16 +0100

    added qr-code scanner

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { View, Text, StyleSheet, Vibration } from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo'
+
 
 export default class LinksScreen extends React.Component {
+  state = {
+    hasCameraPermissions: null
+  }
+
+  async componentWillMound() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({ hasCameraPermission:status === 'granted' })
+  }
+
   static navigationOptions = {
     title: 'Links',
   };
 
+  _handleBarCodeRead = ({type, data}) => {
+    alert(`Type: ${type} and data: ${data}`)
+    Vibration.vibrate(100)
+  }
+
   render() {
+    const { hasCameraPermission } = this.state
+    if(hasCameraPermission === null) {
+      return <Text>Requesting for camera permissions</Text>
+    }
+    if(hasCameraPermission === false ) {
+      return <Text>No access to camera </Text>
+    }
+
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View>
+        <BarCodeScanner
+          onBarCodeRead={this._handleBarCodeRead}
+          style={{ height: 250, width: 350}}
+        />
+      </View>
     );
   }
 }
